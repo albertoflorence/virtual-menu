bc5d51ed369cf951792783d59bcc293f3e3caa96
albertoflorence <alberto.sabatier@gmail.com>
2024-02-20 15:42:02 -0300

    fix build problems

---
+++ app/src/main/java/com/example/trybevirtualmenu/adapters/MenuDataAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.trybevirtualmenu.views
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.
+class MenuDataAdapter (private val menuList: List<MenuData> ) : Adapter<MenuDataAdapter.MenuViewHolder>(){
+
+    class MenuViewHolder(view: View): ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.item_menu_name)
+        val image: TextView = view.findViewById(R.id.item_menu_image)
+        val description: TextView = view.findViewById(R.id.item_menu_description)
+        val price: TextView = view.findViewById(R.id.item_menu_price)
+    }
+
+    override fun getItemCount(): Int = menuList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_menu_layout, parent, false)
+        return MenuViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.name.text = menuList[position].name
+        holder.image.text = menuList[position].image
+        holder.description.text = menuList[position].description
+        holder.price.text = menuList[position].price.toString()
+    }
+
+}
---
+++ app/src/main/java/com/example/trybevirtualmenu/models/MenuData.kt
@@ -0,0 +1,8 @@
+package com.example.trybevirtualmenu.views
+
+data class MenuData(
+    val name: String,
+    val image: String,
+    val description: String,
+    val price: Double
+)
